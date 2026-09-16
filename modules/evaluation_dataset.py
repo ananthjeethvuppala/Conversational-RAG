e928e76f0d0be57b2772ba6d4dd423a6077a13e9
ananthjeethvuppala <ananthjeeth@gmail.com>
2026-09-16 16:33:41 +0530

    Added evaluation_dataset.py

---
+++ modules/evaluation_dataset.py
@@ -0,0 +1,61 @@
+evaluation_dataset = [
+
+    {
+        "question": "What is overfitting?",
+        "expected_sources": [
+            "Machine Learning.pdf"
+        ]
+    },
+
+    {
+        "question": "What is a transformer?",
+        "expected_sources": [
+            "Transformers.pdf"
+        ]
+    },
+
+    {
+        "question": "What is generative AI?",
+        "expected_sources": [
+            "Generative AI.pdf"
+        ]
+    },
+
+    {
+        "question": "What is natural language processing?",
+        "expected_sources": [
+            "Natural Language Processing.pdf"
+        ]
+    },
+
+    {
+        "question": "What is computer vision?",
+        "expected_sources": [
+            "Computer Vision.pdf"
+        ]
+    },
+
+    {
+        "question": "What is deep learning?",
+        "expected_sources": [
+            "Deep Learning.pdf"
+        ]
+    },
+
+    {
+        "question": "What is semantic search?",
+        "expected_sources": [
+            "RAG.pdf"
+        ]
+    },
+
+    {
+        "question": "How are transformers used in large language models?",
+        "expected_sources": [
+            "Transformers.pdf",
+            "Generative AI.pdf",
+            "Large Language Models.pdf"
+        ]
+    }
+
+]
